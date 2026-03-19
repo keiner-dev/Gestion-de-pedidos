0ac1321419675dea9b4bd598ea0f98465cacf119
keiner <ramirezkeiner922@gmail.com>
2026-03-19 14:45:37 -0500

    error de subida

--- reporte1.py
+++
@@ -1,7 +0,0 @@
-def reporte_simple():  # Define la función del reporte simple
-    print("\n--- REPORTE SIMPLE ---")  # Muestra el título del reporte
-    
-    for id_pedido, datos in pedidos.items():  # Recorre cada pedido (ID y datos)
-        print(f"{id_pedido} | Cliente: {datos['cliente_id']} | Total: ${datos['total']:.2f}")  # Imprime info básica
-    
-    print("----------------------")  # Línea final del reporte
