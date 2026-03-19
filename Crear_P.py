e68282ba8d2c8914145a48e189e9387c79c4290b
Isac <isaacalvarez0516@gmail.com>
2026-03-19 14:32:19 -0500

    lo mio

---
+++ Crear_P.py
@@ -0,0 +1,29 @@
+from uso.gestion_datos import cargar_pedidos, guardar_pedidos
+
+def main():
+    """Crea un nuevo pedido y lo guarda."""
+    pedidos = cargar_pedidos()
+    nuevo_id = f"P{len(pedidos) + 1:03d}"
+    print(f"--- Crear Pedido Nuevo (ID: {nuevo_id}) ---")
+
+    try:
+        cliente = input("Nombre del cliente: ")
+        producto = input("Nombre del producto: ")
+        precio = float(input("Precio del producto: "))
+        cantidad = int(input("Cantidad: "))
+    except ValueError:
+        print("\nError: Precio y cantidad deben ser números. El pedido no fue guardado.")
+        return
+
+    pedidos[nuevo_id] = {
+        "cliente": cliente,
+        "producto": producto,
+        "cantidad": cantidad,
+        "total": precio * cantidad
+    }
+    
+    guardar_pedidos(pedidos)
+    print(f"\n¡Pedido {nuevo_id} guardado con éxito!")
+
+if __name__ == "__main__":
+    main()
